c38c3101cc2f16aaab15210f725c508e20d4957f
Sebastian Garces Montoya <sebastian.garces@ceiba.com.co>
2016-04-18 21:14:18 -0500

    Se organiza por funciones el codigo

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# componentes
-Practicas de la materia Desarrollo de Software basado en Componentes
+# Desarrollo de Software Basado en Componentes
+
+## Practica 1
+
+## Hypermedia as the Engine of Application State
+
+* Lenguaje: Python
+* API: [Spotify API]
+
+### Comando
+```sh
+$ py reproducionCancion.py
+```
+
+[Spotify API]: <https://developer.spotify.com/web-api/>
--- reproducionCancion.py
+++ reproducionCancion.py
@@ -2,41 +2,47 @@ import requests
 import json
 import webbrowser
 
-def buscarCancion():
+def buscarAlbum():
+
     banda = input('Ingrese la banda a buscar: ')
     limiteBusqueda = input('Ingrese limite de busqueda: ')
-
     url = 'https://api.spotify.com/v1/search?q=' + banda + '&type=album&limit=' + limiteBusqueda
     respuestaBusquedaBanda = requests.get(url)
-
     dataBanda = json.loads(respuestaBusquedaBanda.text)
-
     urlAlbum = dataBanda['albums']['items']
+    return urlAlbum, dataBanda
 
-    if len(urlAlbum) == 0 :
+def mostrarAlbums(urlAlbum):
+
+    if len(urlAlbum) != 0:
         for i in range(0, len(urlAlbum)):
             print(i, ')', urlAlbum[i]['name'])
-        else:
-            print('No se encuentran albums')
-            buscarCancion()
+    else:
+        print('No se encuentran albums')
+        buscarPreviewCancion()
+
+def mostrarCanciones(dataBanda):
 
     opcionSeleccionadaAlbum = input('Ingrese el numero de su album: ')
-
     urlAlbum = dataBanda['albums']['items'][int(opcionSeleccionadaAlbum)]['href']
-
     respuestaAlbum = requests.get(urlAlbum)
-
-    dataBanda = json.loads(respuestaAlbum.text)
-
-    nombreCancion = dataBanda['tracks']['items']
+    dataAlbum = json.loads(respuestaAlbum.text)
+    nombreCancion = dataAlbum['tracks']['items']
 
     for i in range(0, len(nombreCancion)):
         print(i, ')', nombreCancion[i]['name'])
 
+    return dataAlbum
+
+def previewCancion(dataBanda):
     opcionSeleccionadaCancion = input('Ingrese el numero de su cancion: ')
-
     urlCancion = dataBanda['tracks']['items'][int(opcionSeleccionadaCancion)]['preview_url']
-
     webbrowser.open(urlCancion)
 
-buscarCancion()
+def buscarPreviewCancion():
+    urlAlbum, dataBanda = buscarAlbum()
+    mostrarAlbums(urlAlbum)
+    dataAlbum = mostrarCanciones(dataBanda)
+    previewCancion(dataAlbum)
+
+buscarPreviewCancion()
